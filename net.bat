2f6cb74b100749737bd040613a1397fc9791b587
nyuiop <gapin.sla@gmail.com>
2025-05-16 16:14:57 +0300

    Update net.bat

--- net.bat
+++ net.bat
@@ -7,33 +7,33 @@ cls & cls && timeout 0 && cls & cls && timeout 0 && cls & cls && timeout 0 && cl
 cls & cls && timeout 0 && cls & cls && timeout 0 && cls & cls && timeout 0 && cls & cls && timeout 0
 cls & cls && timeout 0 && cls & cls && timeout 0 && cls & cls && timeout 0 && cls & cls && timeout 0
 
-cls & cls && NET SHARE IPC$ /DELETE
-cls & cls && NET SHARE A$ /DELETE
-cls & cls && NET SHARE B$ /DELETE
-cls & cls && NET SHARE C$ /DELETE
-cls & cls && NET SHARE D$ /DELETE
-cls & cls && NET SHARE E$ /DELETE
-cls & cls && NET SHARE F$ /DELETE
-cls & cls && NET SHARE G$ /DELETE
-cls & cls && NET SHARE H$ /DELETE
-cls & cls && NET SHARE I$ /DELETE
-cls & cls && NET SHARE J$ /DELETE
-cls & cls && NET SHARE K$ /DELETE
-cls & cls && NET SHARE L$ /DELETE
-cls & cls && NET SHARE M$ /DELETE
-cls & cls && NET SHARE N$ /DELETE
-cls & cls && NET SHARE O$ /DELETE
-cls & cls && NET SHARE P$ /DELETE
-cls & cls && NET SHARE Q$ /DELETE
-cls & cls && NET SHARE R$ /DELETE
-cls & cls && NET SHARE S$ /DELETE
-cls & cls && NET SHARE T$ /DELETE
-cls & cls && NET SHARE U$ /DELETE
-cls & cls && NET SHARE V$ /DELETE
-cls & cls && NET SHARE W$ /DELETE
-cls & cls && NET SHARE X$ /DELETE
-cls & cls && NET SHARE Y$ /DELETE
-cls & cls && NET SHARE Z$ /DELETE
+cls & cls && NET SHARE IPC$ /DELETE && cls & cls
+cls & cls && NET SHARE A$ /DELETE && cls & cls
+cls & cls && NET SHARE B$ /DELETE && cls & cls
+cls & cls && NET SHARE C$ /DELETE && cls & cls
+cls & cls && NET SHARE D$ /DELETE && cls & cls
+cls & cls && NET SHARE E$ /DELETE && cls & cls
+cls & cls && NET SHARE F$ /DELETE && cls & cls
+cls & cls && NET SHARE G$ /DELETE && cls & cls
+cls & cls && NET SHARE H$ /DELETE && cls & cls
+cls & cls && NET SHARE I$ /DELETE && cls & cls
+cls & cls && NET SHARE J$ /DELETE && cls & cls
+cls & cls && NET SHARE K$ /DELETE && cls & cls
+cls & cls && NET SHARE L$ /DELETE && cls & cls
+cls & cls && NET SHARE M$ /DELETE && cls & cls
+cls & cls && NET SHARE N$ /DELETE && cls & cls
+cls & cls && NET SHARE O$ /DELETE && cls & cls
+cls & cls && NET SHARE P$ /DELETE && cls & cls
+cls & cls && NET SHARE Q$ /DELETE && cls & cls
+cls & cls && NET SHARE R$ /DELETE && cls & cls
+cls & cls && NET SHARE S$ /DELETE && cls & cls
+cls & cls && NET SHARE T$ /DELETE && cls & cls
+cls & cls && NET SHARE U$ /DELETE && cls & cls
+cls & cls && NET SHARE V$ /DELETE && cls & cls
+cls & cls && NET SHARE W$ /DELETE && cls & cls
+cls & cls && NET SHARE X$ /DELETE && cls & cls
+cls & cls && NET SHARE Y$ /DELETE && cls & cls
+cls & cls && NET SHARE Z$ /DELETE && cls & cls
 
 
 cls & cls && timeout 0 && cls & cls && timeout 0 && cls & cls && timeout 0 && cls & cls && timeout 0
